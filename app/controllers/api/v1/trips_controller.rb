f3728a641e2342b6bd1d590beb4525dfa1a23677
joshpson <jpearson846@gmail.com>
2018-07-06 10:23:35 -0400

    full crud for trips

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,7 +1,38 @@
 class Api::V1::TripsController < ApplicationController
 
+  before_action :find_trip, only: [:update, :show, :destroy]
+
   def index
     @trips = Trip.all
     render json: @trips
   end
+
+  def create
+    @trip = Trip.create(trip_params)
+    render json: @trip, status: :accepted
+  end
+
+  def update
+    @trip.update(trip_params)
+    render json: @trip, status: :accepted
+  end
+
+  def show
+    render json: @trip
+  end
+
+  def destroy
+    @trip.destroy
+  end
+
+  private
+
+  def find_trip
+    @trip = Trip.find_by(id: params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:city, :start_date, :end_date, :address)
+  end
+
 end
